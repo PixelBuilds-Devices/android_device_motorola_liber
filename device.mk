99ef0a6fa3c638955c02e9c5ef126349bf527efa
Michael Bestas <mkbestas@gmail.com>
2022-08-19 20:14:41 +0300

    liber: Build aidl shim libraries for S compatibility
    T changed *ndk_platform.so to *ndk.so.
    Add shim libraries to make old blobs work again.
    
    Change-Id: I77074f1fadbd44a63b6b9e9822632064d164ef94

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     libgui_vendor
 
+# audio.primary.sm6150 shim
+PRODUCT_PACKAGES += \
+    android.hardware.power-V1-ndk_platform.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
