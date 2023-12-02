d78119f4c127e1852b8dc1905aec336cab6b5b78
DhineshCool <dhineshcool585@gmail.com>
2023-12-02 23:56:58 +0530

    liber:update overlay path

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ $(call inherit-product, vendor/motorola/liber/liber-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_PACKAGES += \
     ApertureOverlayLiber \
