c959c83c9b1f3a0714f7081545e42c8435dc0519
DhineshCool <dhineshcool585@gmail.com>
2024-08-17 14:31:08 +0530

    liber: drop stock fingerprint

--- aosp_liber.mk
+++ aosp_liber.mk
@@ -41,9 +41,7 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Build info
-BUILD_FINGERPRINT := "motorola/liber_retail/liber:11/RPIS31.Q2-42-25-1/19a8e:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=liber_retail \
-    PRIVATE_BUILD_DESC="liber_retail-user 11 RPIS31.Q2-42-25-1 19a8e release-keys"
+    PRODUCT_NAME=liber_retail
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
