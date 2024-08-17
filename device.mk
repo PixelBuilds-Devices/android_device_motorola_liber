02889690e94f3c74d58f028af89479635f4fb998
DhineshCool <dhineshcool585@gmail.com>
2024-08-17 14:27:49 +0530

    liber: drop rro_overlay for settings

--- device.mk
+++ device.mk
@@ -28,12 +28,12 @@ $(call inherit-product, vendor/motorola/liber/liber-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_PACKAGES += \
     ApertureOverlayLiber \
     FrameworksResLiber \
-    SettingsResLiber \
     SystemUIResLiber
 
 PRODUCT_PACKAGES += \
